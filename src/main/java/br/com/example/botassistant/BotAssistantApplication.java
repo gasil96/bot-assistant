6ba199cfbefb9b1fe6589fe27d4d091fe16390cb
gasil96 <gasil96@gmail.com>
2023-09-30 14:56:45 -0300

    initial commit this project

---
+++ src/main/java/br/com/example/botassistant/BotAssistantApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.botassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotAssistantApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BotAssistantApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/br/com/example/botassistant/BotAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.botassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotAssistantApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
